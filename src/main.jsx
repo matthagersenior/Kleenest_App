1879552637d926e07ab81f9292bf9d7b52b9244b
matthagersenior <matthagersr@gmail.com>
2026-08-20 02:09:53 -0500

    Switch application entry to build-safe runtime shell

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
+import App from './AppRuntime.jsx';
 import FeatureIntegration from './components/FeatureIntegration';
 import { AuthProvider } from './context/AuthContext';
 import { assertRuntimeConfig } from './lib/runtime';
