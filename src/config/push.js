b68b43cb135eeec9ed63f8cc9ec1d15c94293885
matthagersenior <matthagersr@gmail.com>
2026-08-20 06:27:28 -0500

    Configure browser push VAPID public key

---
+++ src/config/push.js
@@ -0,0 +1,2 @@
+// Browser-safe VAPID public key. Never place the VAPID private key in client code.
+export const VAPID_PUBLIC_KEY='BDzFMNGJ_XMW02VzZicWevYe6esyKx8p0AHpt-_-7aKNHLp4lKE-OHYVjc7TjwUlq0dGBzm3sycq1aAJ152XAYs';
