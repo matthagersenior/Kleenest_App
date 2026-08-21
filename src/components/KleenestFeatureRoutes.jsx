2707ff192723027aa79cf885764ffd1f9561bf61
matthagersenior <matthagersr@gmail.com>
2026-08-20 20:52:43 -0500

    Wire Business Reviews and correct Fleet route authorization

--- src/components/KleenestFeatureRoutes.jsx
+++ src/components/KleenestFeatureRoutes.jsx
@@ -7,9 +7,11 @@ import BusinessEntitlementsPage from '../pages/BusinessEntitlementsPage';
 import BusinessPerformancePage from '../pages/BusinessPerformancePage';
 import BusinessIntelligencePage from '../pages/BusinessIntelligencePage';
 import BusinessManagePage from '../pages/BusinessManagePage';
+import BusinessReviewsPage from '../pages/BusinessReviewsPage';
 import FleetReviewPage from '../pages/FleetReviewPage';
 
 const businessRoles=['business','owner','admin'];
+const fleetRoles=['fleet','admin'];
 
 export default function KleenestFeatureRoutes(){return <>
   <Route path="/rewards" element={<RouteGuard requireAuth><RewardsPage/></RouteGuard>}/>
@@ -17,7 +19,8 @@ export default function KleenestFeatureRoutes(){return <>
   <Route path="/leaderboard" element={<RouteGuard requireAuth><LeaderboardPage/></RouteGuard>}/>
   <Route path="/business/intelligence" element={<RouteGuard requireAuth roles={businessRoles}><BusinessIntelligencePage/></RouteGuard>}/>
   <Route path="/business/manage" element={<RouteGuard requireAuth roles={businessRoles}><BusinessManagePage/></RouteGuard>}/>
-  <Route path="/fleet" element={<RouteGuard requireAuth roles={businessRoles}><FleetReviewPage/></RouteGuard>}/>
+  <Route path="/business/reviews" element={<RouteGuard requireAuth roles={businessRoles}><BusinessReviewsPage/></RouteGuard>}/>
+  <Route path="/fleet" element={<RouteGuard requireAuth roles={fleetRoles}><FleetReviewPage/></RouteGuard>}/>
   <Route path="/business/entitlements" element={<RouteGuard requireAuth roles={businessRoles}><BusinessEntitlementsPage/></RouteGuard>}/>
   <Route path="/business/performance" element={<RouteGuard requireAuth roles={businessRoles}><BusinessPerformancePage/></RouteGuard>}/>
 </>}
