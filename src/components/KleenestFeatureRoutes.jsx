f3e486514650e056201cc6b0eb15bddc901de109
matthagersenior <matthagersr@gmail.com>
2026-08-20 20:53:57 -0500

    Wire Business Dashboard into canonical feature routes

--- src/components/KleenestFeatureRoutes.jsx
+++ src/components/KleenestFeatureRoutes.jsx
@@ -3,6 +3,7 @@ import RouteGuard from './RouteGuard';
 import RewardsPage from '../pages/RewardsPage';
 import ContestsPage from '../pages/ContestsPage';
 import LeaderboardPage from '../pages/LeaderboardPage';
+import BusinessDashboardPage from '../pages/BusinessDashboardPage';
 import BusinessEntitlementsPage from '../pages/BusinessEntitlementsPage';
 import BusinessPerformancePage from '../pages/BusinessPerformancePage';
 import BusinessIntelligencePage from '../pages/BusinessIntelligencePage';
@@ -17,6 +18,7 @@ export default function KleenestFeatureRoutes(){return <>
   <Route path="/rewards" element={<RouteGuard requireAuth><RewardsPage/></RouteGuard>}/>
   <Route path="/contests" element={<RouteGuard requireAuth><ContestsPage/></RouteGuard>}/>
   <Route path="/leaderboard" element={<RouteGuard requireAuth><LeaderboardPage/></RouteGuard>}/>
+  <Route path="/business/dashboard" element={<RouteGuard requireAuth roles={businessRoles}><BusinessDashboardPage/></RouteGuard>}/>
   <Route path="/business/intelligence" element={<RouteGuard requireAuth roles={businessRoles}><BusinessIntelligencePage/></RouteGuard>}/>
   <Route path="/business/manage" element={<RouteGuard requireAuth roles={businessRoles}><BusinessManagePage/></RouteGuard>}/>
   <Route path="/business/reviews" element={<RouteGuard requireAuth roles={businessRoles}><BusinessReviewsPage/></RouteGuard>}/>
