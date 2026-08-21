9c9f627b1b5dea1c1ed0a4e474cc63cd8ac8bd09
matthagersenior <matthagersr@gmail.com>
2026-08-21 17:43:48 -0500

    Mount intelligence notification event bridge

--- src/components/FeatureIntegration.jsx
+++ src/components/FeatureIntegration.jsx
@@ -5,6 +5,7 @@ import PushNotificationBridge from './PushNotificationBridge';
 import NetworkRealtimeBridge from './NetworkRealtimeBridge';
 import NetworkEventToast from './NetworkEventToast';
 import NotificationRealtimeBridge from './NotificationRealtimeBridge';
+import IntelligenceNotificationEventBridge from './IntelligenceNotificationEventBridge';
 
 // Cross-cutting integrations only; application routes remain in AppRuntime.
 export default function FeatureIntegration(){
@@ -16,5 +17,6 @@ export default function FeatureIntegration(){
     <NetworkRealtimeBridge/>
     <NetworkEventToast/>
     <NotificationRealtimeBridge/>
+    <IntelligenceNotificationEventBridge/>
   </>;
 }
